bc4a23bba82e2477cfa02905e0166c0d69c1ed30
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-29 11:45:28 -0700

    add cell event listener

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 console.log('I am connected!')
 const gameBoard = (() => {
-  const array = []
+  const array = Array(9).fill('')
   const getArray = () => array
-  const addItem = (item) => array.push(item)
-
+  const addItem = (item, index) => array[index] = item
+  
   return { getArray, addItem }
 })();
 
 const displayController = (() => {
+  const cell = document.querySelectorAll('.square')
 
+  cell.forEach((element) => {
+    element.addEventListener('click', () => {
+      index = parseInt(element.getAttribute('id')) - 1;
+      gameBoard.addItem('X', index);
+      updateDisplay();
+    });
+  });
 })();
 
 const playerFactory = (name) => {
 
   return { name };
 };
+

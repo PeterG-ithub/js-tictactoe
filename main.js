b56dff977256c9df48fc871c9b3b03bb2e47d0e3
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-29 10:59:09 -0700

    Add 2 module, 1 factory

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log('I am connected!')
+const gameBoard = (() => {
+  const array = []
+  const getArray = () => array
+  const addItem = (item) => array.push(item)
+
+  return { getArray, addItem }
+})();
+
+const displayController = (() => {
+
+})();
+
+const playerFactory = (name) => {
+
+  return { name };
+};
